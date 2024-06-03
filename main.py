aeb0be92d6736509b0751e6342b4dee8efefea0a
liudengfeng <liudengfeng_sd@outlook.com>
2024-06-04 04:16:43 +0800

    web

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, 刘登峰!")))
+    page.add(ft.SafeArea(ft.Text("Hello, 中国!")))
 
 
-ft.app(main)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
